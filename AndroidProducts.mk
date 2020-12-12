ecee591f5a068334f6e866ff9061e0de4d99fa67
Loïc Poisot <elpoisot@hotmail.fr>
2020-12-12 16:10:29 +0100

    increased lisibility of maru specific changes
    Signed-off-by: Loïc Poisot <elpoisot@hotmail.fr>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -3,6 +3,8 @@ PRODUCT_MAKEFILES := \
 	$(LOCAL_DIR)/aosp_marlin_svelte.mk \
 	$(LOCAL_DIR)/aosp_sailfish.mk \
 	$(LOCAL_DIR)/lineage_marlin.mk \
-	$(LOCAL_DIR)/lineage_sailfish.mk \
+	$(LOCAL_DIR)/lineage_sailfish.mk
+
+PRODUCT_MAKEFILES += \
 	$(LOCAL_DIR)/maru_marlin.mk \
 	$(LOCAL_DIR)/maru_sailfish.mk
